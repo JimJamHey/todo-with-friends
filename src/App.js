2edb48f553cbd88dd190187acf40aefa183ab582
JimJamHey <jarensmart@yahoo.com>
2022-12-15 16:31:21 -0500

    added image and some styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import handImage from "./images/hand-image.png";
 
 function App() {
   return (
     <div className="App-container">
+      <img
+        alt="a hand gripping a pencil"
+        className="hand-image"
+        src={handImage}
+      />
       <header className="App-header">Today's Tasks</header>
     </div>
   );
