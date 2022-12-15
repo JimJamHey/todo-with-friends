531b164057c68fdf8ce831c3e30aaefe366c108e
JimJamHey <jarensmart@yahoo.com>
2022-12-15 15:31:26 -0500

    cra initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App-container">
+      <header className="App-header">Today's Tasks</header>
+    </div>
+  );
+}
+
+export default App;
